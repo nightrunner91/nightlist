1cc904ade4e4553557f2f86d378de17b102fe1a4
Nikita <n.semichin@musqogee.com>
2021-02-16 13:37:30 +0200

    Added initial collection adding action

--- src/views/pages/Dashboard.vue
+++ src/views/pages/Dashboard.vue
@@ -144,6 +144,10 @@ export default {
       return this.$store.state.collection.filter(i => i.category == 'books')
     },
 
+    collection() {
+      return this.$store.state.collection
+    },
+
     totalLength() {
       return this.$store.state.collection.length
     },
@@ -152,52 +156,62 @@ export default {
       return this.$store.state.modalState
     }
   },
+  watch: {
+    collection(oldVal, newVal) {
+      this.prepareData()
+    }
+  },
   methods: {
     prepareData() {
       // Store global varaible
       let summary = this.platesData.summary
 
-      // Scan each category
-      for (let index = 0; index < summary.length; index++) {
-        // Store collection and needed params
-        let collection = this.$store.state.collection.filter(i => i.category == summary[index].id)
-        let params = { hours: [], entries: [], favourites: [] }
+      // Prepare data if there is some data
+      if (this.$store.state.collection.length > 0) {
 
-        // Pish params object arrays with values from each category
-        for (let index = 0; index < collection.length; index++) {
-          params.hours.push(parseFloat(collection[index].hours))
+        // Scan each category
+        for (let index = 0; index < summary.length; index++) {
+          // Store collection and needed params
+          let collection = this.$store.state.collection.filter(i => i.category == summary[index].id)
+          let params = { hours: [], entries: [], favourites: [] }
+
+          // Pish params object arrays with values from each category
+          for (let index = 0; index < collection.length; index++) {
+            params.hours.push(parseFloat(collection[index].hours))
+          }
+          params.entries.push(parseFloat(collection.length))
+          params.favourites.push(parseFloat(collection.filter(i => i.favourite).length))
+
+          // Store summ of each category in data()
+          Object.keys(params).forEach(key => {
+            summary[index][key] = parseFloat((params[key].reduce(reducer)).toFixed(0))
+          })
         }
-        params.entries.push(parseFloat(collection.length))
-        params.favourites.push(parseFloat(collection.filter(i => i.favourite).length))
 
-        // Store summ of each category in data()
-        Object.keys(params).forEach(key => {
-          summary[index][key] = parseFloat((params[key].reduce(reducer)).toFixed(0))
+        // Calculate totals
+        let totals = { hours: [], entries: [], favourites: [] }
+
+        for (let index = 0; index < summary.length; index++) {
+          totals.hours.push(parseFloat(summary[index].hours))
+          totals.entries.push(parseFloat(summary[index].entries))
+          totals.favourites.push(parseFloat(summary[index].favourites))
+        }
+
+        Object.keys(totals).forEach(key => {
+          this.platesData.total[key] = parseFloat((totals[key].reduce(reducer)).toFixed(0))
         })
-      }
 
-      // Calculate totals
-      let totals = { hours: [], entries: [], favourites: [] }
+        // Calculate percentages
+        function perc(value, total) {
+          return parseFloat((value * 100 / total).toFixed(0))
+        }
 
-      for (let index = 0; index < summary.length; index++) {
-        totals.hours.push(parseFloat(summary[index].hours))
-        totals.entries.push(parseFloat(summary[index].entries))
-        totals.favourites.push(parseFloat(summary[index].favourites))
-      }
+        for (let index = 0; index < summary.length; index++) {
+          summary[index].hoursPercent = perc(summary[index].hours, this.platesData.total.hours)
+          summary[index].entriesPercent = perc(summary[index].entries, this.platesData.total.entries)
+          summary[index].favouritesPercent = perc(summary[index].favourites, this.platesData.total.favourites)
+        }
 
-      Object.keys(totals).forEach(key => {
-        this.platesData.total[key] = parseFloat((totals[key].reduce(reducer)).toFixed(0))
-      })
-
-      // Calculate percentages
-      function perc(value, total) {
-        return parseFloat((value * 100 / total).toFixed(0))
-      }
-
-      for (let index = 0; index < summary.length; index++) {
-        summary[index].hoursPercent = perc(summary[index].hours, this.platesData.total.hours)
-        summary[index].entriesPercent = perc(summary[index].entries, this.platesData.total.entries)
-        summary[index].favouritesPercent = perc(summary[index].favourites, this.platesData.total.favourites)
       }
     }
   },
