e09e0958de24ea0a0b472b4f121b028ed8e0cff2
Nikita <semikin91@gmail.com>
2022-06-23 15:04:59 +0300

    changed route

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:nightrunner91/nightlist-frontend.git master:gh-pages
+git push -f git@github.com:nightrunner91/nightlist.git master:gh-pages
 
 cd -
